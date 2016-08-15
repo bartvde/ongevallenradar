83e5d1c4158bc1b8209f8becc5410339223d8b3c
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-08-15 21:08:57 +0200

    Add initial labeling

--- app.js
+++ app.js
@@ -1,59 +1,49 @@
-var styles = {
-  'een': new ol.style.Style({
-    image: new ol.style.RegularShape({
-      fill: new ol.style.Fill({color: '#FF0000'}),
-      stroke: new ol.style.Stroke({color: '#8a0808', width: 2}),
-      points: 3,
-      radius: 15,
-      angle: 0
-    })
+var imageStyles = {
+  'een': new ol.style.RegularShape({
+    fill: new ol.style.Fill({color: '#FF0000'}),
+    stroke: new ol.style.Stroke({color: '#8a0808', width: 2}),
+    points: 3,
+    radius: 15,
+    angle: 0
   }),
-  'twee': new ol.style.Style({
-    image: new ol.style.RegularShape({
-      fill: new ol.style.Fill({color: '#FE9A2E'}),
-      stroke: new ol.style.Stroke({color: '#B45F04', width: 2}),
-      points: 3,
-      radius: 15,
-      angle: 0
-    })
+  'twee': new ol.style.RegularShape({
+    fill: new ol.style.Fill({color: '#FE9A2E'}),
+    stroke: new ol.style.Stroke({color: '#B45F04', width: 2}),
+    points: 3,
+    radius: 15,
+    angle: 0
   }),
-  'drie': new ol.style.Style({
-    image: new ol.style.RegularShape({
-      fill: new ol.style.Fill({color: '#FE9A2E'}),
-      stroke: new ol.style.Stroke({color: '#B45F04', width: 2}),
-      points: 3,
-      radius: 15,
-      angle: 0
-    })
+  'drie': new ol.style.RegularShape({
+    fill: new ol.style.Fill({color: '#FE9A2E'}),
+    stroke: new ol.style.Stroke({color: '#B45F04', width: 2}),
+    points: 3,
+    radius: 15,
+    angle: 0
   }),
-  'vier': new ol.style.Style({
-    image: new ol.style.RegularShape({
-      fill: new ol.style.Fill({color: '#F4FA58'}),
-      stroke: new ol.style.Stroke({color: '#AEB404', width: 2}),
-      points: 3,
-      radius: 15,
-      angle: 0
-    })
+  'vier': new ol.style.RegularShape({
+    fill: new ol.style.Fill({color: '#F4FA58'}),
+    stroke: new ol.style.Stroke({color: '#AEB404', width: 2}),
+    points: 3,
+    radius: 15,
+    angle: 0
   }),
-  'vijf': new ol.style.Style({
-    image: new ol.style.RegularShape({
-      fill: new ol.style.Fill({color: '#F4FA58'}),
-      stroke: new ol.style.Stroke({color: '#AEB404', width: 2}),
-      points: 3,
-      radius: 15,
-      angle: 0
-    })
+  'vijf': new ol.style.RegularShape({
+    fill: new ol.style.Fill({color: '#F4FA58'}),
+    stroke: new ol.style.Stroke({color: '#AEB404', width: 2}),
+    points: 3,
+    radius: 15,
+    angle: 0
   }),
-  'zes': new ol.style.Style({
-    image: new ol.style.RegularShape({
-      fill: new ol.style.Fill({color: '#F4FA58'}),
-      stroke: new ol.style.Stroke({color: '#AEB404', width: 2}),
-      points: 3,
-      radius: 15,
-      angle: 0
-    })
+  'zes': new ol.style.RegularShape({
+    fill: new ol.style.Fill({color: '#F4FA58'}),
+    stroke: new ol.style.Stroke({color: '#AEB404', width: 2}),
+    points: 3,
+    radius: 15,
+    angle: 0
   })
 };
+
+var styleCache = {};
 
 var map = new ol.Map({
   layers: [
@@ -65,7 +55,20 @@ var map = new ol.Map({
     new ol.layer.Vector({
       style: function(feature, resolution) {
         var nummer = feature.get('nummer');
-        return styles[nummer];
+        var text = feature.get('bps') + '\n' + feature.get('tijdstip') + '\n' + feature.get('incident_type');
+        if (!styleCache[nummer + '|' + text]) {
+          styleCache[nummer + '|' + text] = new ol.style.Style({
+            text: new ol.style.Text({
+              fill: new ol.style.Fill({color: '#0000FF'}),
+              stroke: new ol.style.Stroke({color: '#FFFFFF', width: 1.5}),
+              font: 'bold 11px Arial',
+              offsetY: -35,
+              text: text
+            }),
+            image: imageStyles[nummer]
+          });
+        }
+        return styleCache[nummer + '|' + text];
       },
       source: new ol.source.Vector({
         strategy: ol.loadingstrategy.all,
